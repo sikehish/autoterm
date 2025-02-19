0abc063ad91f8d4de1869d32e2da4d237d58403f
sikehish <hisham0502@gmail.com>
2025-02-19 23:45:57 +0530

    Fixed a minor bug related to the path of index.html and added more robust error handling and enhanced terminal creation process(prorotype)

--- src/extension.ts
+++ src/extension.ts
@@ -4,42 +4,45 @@ import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand("autoterm.openWebview", () => {
-      const panel = vscode.window.createWebviewPanel(
-        "autoterm",
-        "AutoTerm",
-        vscode.ViewColumn.One,
-        { enableScripts: true }
-      );
-  
-      const scriptUri = panel.webview.asWebviewUri(
-        vscode.Uri.file(path.join(context.extensionPath, "dist", "script.js"))
-      );
-  
-      const styleUri = panel.webview.asWebviewUri(
-        vscode.Uri.file(path.join(context.extensionPath, "src", "webview", "styles.css"))
-      );
-  
-      panel.webview.html = getWebviewContent(scriptUri, styleUri);
+        const panel = vscode.window.createWebviewPanel(
+            "autoterm",
+            "AutoTerm",
+            vscode.ViewColumn.One,
+            { enableScripts: true } 
+        );
+
+        const scriptUri = panel.webview.asWebviewUri(
+            vscode.Uri.file(path.join(context.extensionPath, "dist", "script.js"))
+        );
+
+        const styleUri = panel.webview.asWebviewUri(
+            vscode.Uri.file(path.join(context.extensionPath, "src", "webview", "styles.css"))
+        );
+
+        panel.webview.html = getWebviewContent(context);
+
         panel.webview.onDidReceiveMessage(
-        (message) => {
-          if (message.command === "showAlert") {
-            vscode.window.showInformationMessage(message.text);
-          }
-        },
-        undefined,
-        context.subscriptions
-      );
+            (message) => {
+                if (message.command === "saveTerminals") {
+                  if(message.data.length === 0)  vscode.window.showErrorMessage("CAN'T HAVE 0 TERMINALS!")
+                  else vscode.window.showInformationMessage(`Saved ${message.data.length} ${((message.data.length)===1) ?'terminal!' :  'terminals!'} `);
+                    console.log("Received Terminals Data:", message.data);
+                }
+            },
+            undefined,
+            context.subscriptions
+        );
     });
+
     context.subscriptions.push(disposable);
-  }
-  
+}
 
 export function deactivate() {}
 
-function getWebviewContent(scriptUri: vscode.Uri, styleUri: vscode.Uri): string {
-  const html_to_send = fs.readFileSync(
-   '/Users/afnan/Developer/autoterm/src/webview/index.html',
-    'utf8'
-  );
-  return html_to_send;
+function getWebviewContent(context: vscode.ExtensionContext): string {
+    const html_to_send = fs.readFileSync(
+        path.join(context.extensionPath, "src", "webview", "index.html"),
+        "utf8"
+    );
+    return html_to_send;
 }
